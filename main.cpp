fe6a9c566e01cd68ced20d2e42f590e3150ce04b
samuel <samuel.alvesg@hotmail.com>
2021-07-02 08:18:15 -0300

    ajuste

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@
 
 int main(int argc, char *argv[])
 {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
+
 
     QGuiApplication app(argc, argv);
+
+    // Registrando objeto para que possa ser usado no QML
     qmlRegisterType<Counter>("Counter", 1, 0, "Counter");
+
+
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
